fa0737ae1a703e4f4065699c85c71f8807bb6bdc
cch137 <96408836+cch137@users.noreply.github.com>
2024-07-04 16:46:33 +0800

    update bin

--- bin/publish.py
+++ bin/publish.py
@@ -3,10 +3,14 @@ import shutil
 
 shutil.rmtree('dist', ignore_errors=True)
 os.system('tsc')
-shutil.copyfile('.npmignore', 'dist/.npmignore')
+try: shutil.copyfile('.npmignore', 'dist/.npmignore')
+except: pass
+try: shutil.copyfile('README.md', 'dist/README.md')
+except: pass
 shutil.copyfile('package.json', 'dist/package.json')
 
 os.system(' && '.join([
   'cd dist/',
   'npm publish --access public',
 ]))
+os.remove('dist/package.json')
